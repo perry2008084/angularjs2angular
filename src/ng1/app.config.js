507f691088be8ad878c7c0493a20bf7bb470a4e0
perry <perry2008084@gmail.com>
2018-02-07 13:55:16 +0800

    修改部分路由无法访问的路径设置

--- src/ng1/app.config.js
+++ src/ng1/app.config.js
@@ -10,8 +10,11 @@ angular.
         when('/phones', {
           template: '<phone-list></phone-list>'
         }).
-        when('/phones/:phoneId', {
+        when('/ng1/phones/:phoneId', {
           template: '<phone-detail></phone-detail>'
+        }).
+        when('/ng2-demo', {
+          template: '<ng2-demo></ng2-demo>'
         }).
         otherwise('/phones');
     }
